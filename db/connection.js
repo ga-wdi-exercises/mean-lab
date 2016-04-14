d882ad77cda71ffe8f1898099b8f75e3b2be3079
Charles <charlesthwang@gmail.com>
2016-04-14 10:54:12 -0400

    Connected to database

---
+++ db/connection.js
@@ -0,0 +1,11 @@
+var mongoose = require("mongoose");
+
+var MinionSchema = {
+  name: String,
+  eyes: Number,
+  hair: String,
+  body: String
+}
+
+mongoose.model("Minion", MinionSchema);
+mongoose.connect("mongodb://localhost/minions")

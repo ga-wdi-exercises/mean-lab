b2b68affb4fd66a71a1b0c6b36a956fdb7cd5cdd
Charles <charlesthwang@gmail.com>
2016-04-14 23:34:02 -0400

    Added second model nested under the first model

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,24 @@
 var mongoose = require("mongoose");
 
-var MinionSchema = {
+var Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId
+
+var AboutSchema = new Schema({
+  likes: String,
+  dislikes: String
+});
+
+var MinionSchema = new Schema({
   name: String,
   eyes: Number,
   hair: String,
-  body: String
-}
+  body: String,
+  about: [AboutSchema]
+});
 
 var Minion = mongoose.model("Minion", MinionSchema);
+var AboutModel = mongoose.model("About", AboutSchema);
+
 if(process.env.NODE_ENV == "production"){
   mongoose.connect(process.env.MONGODB_URI);
 }else{

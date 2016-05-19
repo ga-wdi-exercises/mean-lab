822cc6fcf7faf328c42a4325c466e8ce05569403
Joshua <Joshua.Nezianya@gmail.com>
2016-05-19 15:02:42 -0400

    fixed bug

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 var express = require("express");
+var hbs= require("express-handlebars");
+var mongoose= require("./db/connection");
 
 var app     = express();
+var Posts = mongoose.model("Post");
 
 app.get("/", function(req, res){
   res.send("This is working!");
 });
+
+app.set("view engine", "hbs");
+ app.engine(".hbs", hbs({
+   extname:      ".hbs",
+   layoutsDir:   "views",
+   partialsDir:  "views",
+   defaultLayout:"layout-main"
+ }));
+
+
+app.get("/api/posts", function(req, res){
+  Post.find().then(function(posts){
+    res.json(posts);
+  });
+});
+
 
 app.listen(3001, function(){
   console.log("I'm working!");

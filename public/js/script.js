c7278f7b44c7c92bf6a7107573e584d3eef301d4
Michael Nahum <USGrant63@gmail.com>
2016-05-19 14:55:13 -0400

    successfully retrieved episodes from db

--- public/js/script.js
+++ public/js/script.js
@@ -6,18 +6,34 @@
     "ui.router",
     "ngResource"
   ])
-  .config(Router);
+  .config(Router)
+  .factory("Episode", EpisodeFactory)
+  .controller("Index", IndexCtrl);
 
   Router.$inject = ["$stateProvider"];
   function Router($stateProvider){
     $stateProvider
     .state("index", {
       url: "/",
-      templateUrl: "/assets/html/episodes-index.html"
+      templateUrl: "/assets/html/episodes-index.html",
+      controller: "Index",
+      controllerAs: "IndexVm"
     })
     .state("show", {
       url: "/:title",
       templateUrl: "/assets/html/episode-show.html"
     });
+  }
+
+  EpisodeFactory.$inject = ["$resource"];
+  function EpisodeFactory($resource){
+    var Episode = $resource("/api/episodes/:title")
+    return Episode
+  }
+
+  IndexCtrl.$inject = ["Episode"];
+  function IndexCtrl(Episode){
+    var vm      = this;
+    vm.episodes = Episode.query();
   }
 })();

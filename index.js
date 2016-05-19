9f65b46b35f371ae2a0b1f57d361dd39c1f46e58
Michael Nahum <USGrant63@gmail.com>
2016-05-19 16:20:32 -0400

    almost there, just did update

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 var express  = require("express");
 var hbs      = require("express-handlebars");
+var parser   = require("body-parser")
 var mongoose = require("./db/connection");
 var app      = express();
 var Episode  = mongoose.model("Episode");
 
+app.use(parser.json({extended: true}));
 app.use("/assets", express.static("public"));
 app.set("view engine", "hbs");
 app.engine(".hbs", hbs({
   extname:      ".hbs",
   layoutsDir:   "views",
   partialsDir:  "views",
-  defaultLayout:"layout-main"
+  defaultLayout:"main"
 }));
-
-app.get("/", function(req, res){
-  res.render("main", {layout: false});
-});
 
 app.get("/api/episodes", function(req, res){
   Episode.find().then(function(episodes){
-    console.log(episodes)
     res.json(episodes);
   });
+});
+
+app.get("/api/episodes/:_id", function(req, res){
+  Episode.findOne(req.params).then(function(episode){
+    res.json(episode);
+  });
+});
+
+app.put("/api/episodes/:_id", function(req, res){
+  Episode.findOneAndUpdate(req.params, req.body, {new: true}).then(function(episode){
+    res.json(episode);
+  });
+});
+
+app.post("/api/episodes", function(req, res){
+  Episode.create(req.body).then(function(episode){
+    res.json(episode);
+  });
+});
+
+app.get("/*", function(req, res){
+  res.render("main", {layout: false});
 });
 
 app.listen(3001, function(){
--- public/js/script.js
+++ public/js/script.js
@@ -2,16 +2,19 @@
 
 (function(){
   angular
-  .module("ThroneCast", [
+  .module("throneCast", [
     "ui.router",
     "ngResource"
   ])
   .config(Router)
   .factory("Episode", EpisodeFactory)
-  .controller("Index", IndexCtrl);
+  .controller("Index", IndexCtrl)
+  .controller("Show", ShowCtrl)
 
-  Router.$inject = ["$stateProvider"];
-  function Router($stateProvider){
+
+  Router.$inject = ["$stateProvider", "$locationProvider"];
+  function Router($stateProvider, $locationProvider){
+    $locationProvider.html5Mode(true);
     $stateProvider
     .state("index", {
       url: "/",
@@ -20,20 +23,42 @@
       controllerAs: "IndexVm"
     })
     .state("show", {
-      url: "/:title",
-      templateUrl: "/assets/html/episode-show.html"
+      url: "/:_id",
+      templateUrl: "/assets/html/episode-show.html",
+      controller: "Show",
+      controllerAs: "ShowVm"
     });
   }
 
   EpisodeFactory.$inject = ["$resource"];
   function EpisodeFactory($resource){
-    var Episode = $resource("/api/episodes/:title")
-    return Episode
+    var Episode = $resource("/api/episodes/:_id", {}, {
+      update: {method: "PUT"}
+    });
+    return Episode;
   }
 
   IndexCtrl.$inject = ["Episode"];
   function IndexCtrl(Episode){
     var vm      = this;
     vm.episodes = Episode.query();
+    vm.create = function(){
+      Episode.save(vm.newEpisode, function(response){
+        console.log(response)
+        vm.episodes.push(response);
+      });
+    }
   }
+
+  ShowCtrl.$inject = ["Episode", "$stateParams"];
+  function ShowCtrl(Episode, $stateParams){
+    var vm = this;
+    vm.episode = Episode.get($stateParams);
+    vm.update = function(){
+      Episode.update($stateParams, vm.episode, function(response){
+        console.log(response)
+      });
+    }
+  }
+
 })();

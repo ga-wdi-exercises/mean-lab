0e3c9d9be2b9cd9bf5a4ac996209e47f573e7da9
Joshua <Joshua.Nezianya@gmail.com>
2016-05-20 08:55:25 -0400

    not done

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
 var express = require("express");
 var hbs= require("express-handlebars");
+var parser  = require("body-parser");
 var mongoose= require("./db/connection");
 
 var app     = express();
 var Posts = mongoose.model("Post");
 
 app.get("/", function(req, res){
-  res.render("layout-main",{layout:false});
+  res.render("main",{layout:false});
 });
 
+app.use(parser.json({extended: true}));
 app.use("/cool", express.static("public"));
 app.set("view engine", "hbs");
  app.engine(".hbs", hbs({
    extname:      ".hbs",
    layoutsDir:   "views",
    partialsDir:  "views",
-   defaultLayout:"layout-main"
+   defaultLayout:"main"
  }));
 
 
 app.get("/api/posts", function(req, res){
-  Post.find().then(function(posts){
-    res.json(posts);
+  res.json(req.body);
   });
-});
+
 
 
 app.listen(3001, function(){
--- public/script.js
+++ public/script.js
@@ -6,18 +6,34 @@
     "ui.router",
     "ngResource"
   ])
-  .config(Router);
+  .config(Router)
+  .factory("Post", PostFactory)
+  .controller("Index", IndexCtrl);
 
   Router.$inject = ["$stateProvider"];
   function Router($stateProvider){
     $stateProvider
     .state("index", {
-      url: "/",
-      templateUrl: "cool/posts-index.html"
+      url:          "/",
+      templateUrl:  "cool/html/posts-index.html",
+      controller:   "Index",
+      controllerAs: "IndexVM"
     })
     .state("show",  {
-      url: "/:title",
-      templateUrl: "cool/posts-show.html"
+      url:          "/:title",
+      templateUrl:  "/cool/html/posts-show.html"
     });
+  }
+
+  PostFactory.$inject = ["$resource"];
+  function PostFactory($resource){
+    var Post = $resource("/api/posts/:title");
+    return Post;
+  }
+
+  IndexCtrl.$inject = ["Post"];
+  function IndexCtrl(Post){
+    var vm        = this;
+    vm.posts  = Post.query();
   }
 })();
